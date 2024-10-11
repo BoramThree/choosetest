623e06007f5f5954c3f128539d266c1efcbb1a4f
rlaskarb <rlaskarb20@gmail.com>
2024-10-11 16:54:03 +0900

    10/11 nam

---
+++ chap00-choosetest-lecture-source/src/main/java/com/boram/section01/dynamic/NamView.java
@@ -0,0 +1,16 @@
+package com.boram.section01.dynamic;
+
+import java.util.Scanner;
+
+public class NamView {
+
+    public static void  display(){
+
+        Scanner sc = new Scanner(System.in);
+        MenuServiceNam menuServiceNam = new MenuServiceNam();
+
+
+    }
+
+
+}
