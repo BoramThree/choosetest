346599e1803be2a3933f29d4585285fabb963230
jin <jin200126@gmail.com>
2024-10-11 16:31:51 +0900

    241011_add

---
+++ chap00-choosetest-lecture-source/src/main/java/com/boram/section01/dynamic/MenuServiceYejin.java
@@ -0,0 +1,31 @@
+package com.boram.section01.dynamic;
+
+import com.boram.common.MenuDTOYejin;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import java.util.IllegalFormatCodePointException;
+import java.util.List;
+
+import static com.boram.common.Template.getSqlSession;
+
+public class MenuServiceYejin {
+
+    private DynamicSqlMapperYejin mapperYejin;
+
+    public void selectOrderable(String input) {
+        SqlSession sqlSession = getSqlSession();
+
+        mapperYejin = sqlSession.getMapper(DynamicSqlMapperYejin.class);
+
+        List<MenuDTOYejin> menuList = mapperYejin.selectOrderable(input);
+
+        if (menuList != null && menuList.size() > 0){
+            for (MenuDTOYejin menu : menuList){
+                System.out.println(menu);
+            }
+        } else {
+            System.out.println("출력할 결과가 없습니다.");
+        }
+    }
+}
